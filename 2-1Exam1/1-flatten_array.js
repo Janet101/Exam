c95bf32308ff5558935d0172732cf7f18a8ef1c4
Janet <janetwang101@gmail.com>
2019-07-22 19:40:39 -0700

    update

--- 2-1Exam1/1-flatten_array.js
+++ 2-1Exam1/1-flatten_array.js
@@ -4,21 +4,21 @@ let result=flatten(array);
 console.log(result);
 
 /*bfs：从上到下*/
-// function flatten(arr){
-//     res = []
-//     while(arr.length){
-//         let pop = arr.shift(1)//shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
-//         /*如果这个元素是数字类型，就把这个元素放到res中*/
-//         if (typeof pop === "number"){
-//             res.push(pop)
-//         }
-//         /*如果不是数字类型，就把它拆掉最外面一层数组，放到arr的后面*/
-//         else{
-//             arr.push.apply(arr,pop)
-//         }
-//     }
-//     return res
-// }
+ function flatten(arr){
+     res = []
+     while(arr.length){
+         let pop = arr.shift(1)//shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+         /*如果这个元素是数字类型，就把这个元素放到res中*/
+         if (typeof pop === "number"){
+             res.push(pop)
+         }
+         /*如果不是数字类型，就把它拆掉最外面一层数组，放到arr的后面*/
+         else{
+             arr.push.apply(arr,pop)
+         }
+     }
+     return res
+ }
 
 //dfs:从左到右，从上到下
 function flatten(arr) {
